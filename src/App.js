711117c210ba59f507a37e869640ff690e72a066
Will Carter <will.carter@fergusllc.com>
2019-10-30 14:36:13 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { VictoryBar, VictoryTheme, VictoryTooltip } from "victory";
 
-function App() {
+export const App = () => {
+
+  //get a deep copy of default theme. Is there better way to do this?
+  const customTheme = JSON.parse(JSON.stringify(VictoryTheme.grayscale)); 
+  
+  //set the padding property on bar labels to 0
+  customTheme.bar.style.labels.padding = 0;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{width:"350px", margin: '100px', border:"1px solid #ccc"}}>
+        <VictoryBar
+          data={[{ x: 1, y: 1}, { x: 2, y: 2}, {x: 3, y: 3 } ]}
+          style={{ labels: { fill: "black", fontSize: 12} }}
+          labels={({ datum }) => `(${datum.x}, ${datum.y})`}
+          labelComponent={<VictoryTooltip />}
+          theme={customTheme} //use the customTheme from above
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
